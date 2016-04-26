14a8b4aeddee2339d46fa99af1e43c06afb0878d
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2016-04-27 00:14:43 +0100

    Update documentation

--- +dk/+ui/alphashape.m
+++ +dk/+ui/alphashape.m
@@ -1,4 +1,12 @@
 function [h,A] = alphashape( X, varargin )
+%
+% This method is appropriate if the set of points in X (which should be nx3) represents a VOLUME SCATTER.
+% If the set of points represent a surface, which you want to triangulate, you should use dk.ui.triangulation instead.
+%
+% An alternative to this method is dk.ui.convhull (less flexible).
+% See also alphaShape in Matlab.
+%
+% JH
 
     assert( size(X,2)==3, 'Input should be nx3' );
     
--- +dk/+ui/triangulation.m
+++ +dk/+ui/triangulation.m
@@ -1,6 +1,14 @@
 function [h,D] = triangulation( X, varargin )
 %
-% from http://uk.mathworks.com/matlabcentral/fileexchange/5105-making-surface-plots-from-scatter-data
+% Source: http://uk.mathworks.com/matlabcentral/fileexchange/5105-making-surface-plots-from-scatter-data
+%
+% This method is appropriate if the set of points in X (which should be nx3) represents a SURFACE in space.
+% If the set of points are scattered within a volume, from which you want to extract a surface, you should
+% use either dk.ui.convhull or dk.ui.alphashape instead.
+%
+% See also delaunayTriangulation and trisurf in Matlab.
+%
+% JH
 
     assert( size(X,2)==3, 'Input should be nx3.' );
     
