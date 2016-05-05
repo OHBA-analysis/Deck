9553a395a00e64212a1acdf07161d3a2bc18ec7a
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2016-05-05 20:18:26 +0100

    make behaviour clearer

--- +dk/+util/structvals.m
+++ +dk/+util/structvals.m
@@ -2,18 +2,18 @@ function v = structvals( s )
 % v = STRUCTVALS( s )
 %
 % Returns a N x M cell-array where N is the size of the input struct-array and M is the number of fields.
-% Cell (i,j) contains the value of i^th field in the j^th structure.
+% Cell (i,j) contains the value of j^th field in the i^th structure.
 %
 % JH
 
-    n = numel(s);
     f = fieldnames(s);
-    m = numel(f);
-    v = cell( m, n );
+    nstruct = numel(s);
+    nfields = numel(f);
+    v = cell( nstruct, nfields );
     
-    for i = 1:n
-    for j = 1:m
-        v{j,i} = s(i).(f{j});
+    for i = 1:nstruct
+    for j = 1:nfields
+        v{i,j} = s(i).(f{j});
     end
     end
 
