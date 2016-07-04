f9031d5acabddef18bd76b07d7bb4e0625ebcab5
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2016-07-04 18:00:24 +0100

    Add doc and checks

--- +dk/+util/repstruct.m
+++ +dk/+util/repstruct.m
@@ -1,5 +1,15 @@
 function s = repstruct( fields, varargin )
-    
+%
+% s = repstruct( fields, varargin )
+%
+% Create a struct-array with specified fields.
+% This is equivalent to repmat( struct('field1',[],'field2',[],...), varargin{:} ).
+%
+% JH
+
+    assert( iscellstr(fields), 'Expects a cell of fieldnames in input.' );
+    assert( nargin > 1, 'Expects an array size in input.' );
+
     n = numel(fields);
     f = cell(1,2*n);
     f(1:2:end) = fields;
