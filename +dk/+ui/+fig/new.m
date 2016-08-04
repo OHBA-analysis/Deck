cc0e2efddfd573616a73d11493eb824f5f9e61d3
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2016-08-04 13:37:40 +0100

    Add comment

--- +dk/+ui/+fig/new.m
+++ +dk/+ui/+fig/new.m
@@ -1,4 +1,15 @@
 function f = new( name, figsize, screen, varargin )
+%
+% f = new( name, figsize, screen, varargin )
+%
+%     name : name of the new figure
+%  figsize : size of the figure in pixels or normalised units
+%   screen : screen in which the figure should be moved to
+% varargin : additional arguments forwarded to Figure
+%
+%        f : figure handle
+%
+% JH
 
     f = figure( 'name', name, varargin{:} );
     if nargin > 2 && ~isempty(screen)
