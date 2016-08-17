6c39304c2c88183b7b5cea4d6321d9f707c2baf7
Jonathan H <Jonathan.hadida@dtc.ox.ac.uk>
2016-08-17 03:32:44 +0100

    Make it easier to compile mex files directly

--- +dk/+mx/compile_mex.m
+++ +dk/+mx/compile_mex.m
@@ -1,6 +1,10 @@
 function compile_mex( comp, varargin )
 
-    comp.mex_file = true;
-    dk.mx.compile( comp, varargin{:} );
+    if nargin == 1 && ischar(comp)
+        dk.mx.compile( dk.mx.compiler(), comp );
+    else
+        comp.mex_file = true;
+        dk.mx.compile( comp, varargin{:} );
+    end
     
 end
