b64f9c24554d40e44a024dd72f4e56e75cbbe820
Jonathan H <Jonathan.hadida@dtc.ox.ac.uk>
2016-08-17 14:38:52 +0100

    Show PID of nohup job

--- +dk/+mapred/python/mapred_build.py
+++ +dk/+mapred/python/mapred_build.py
@@ -151,10 +151,12 @@ if [ $$# -lt 1 ]; then
     echo "Usage: runworker <WorkerID>"
 fi
 
-nohup nice \\
-    matlab -singleCompThread -nodisplay -r "cd '${startdir}'; startup; cd '${workdir}'; obj = ${classname}(); obj.run_worker('${savedir}',$$1); exit;" \\
-    >| "${logdir}/runworker_$${1}.log" 2>&1 &
+pid=$$(nohup nice \\
+    matlab -singleCompThread -nodisplay \\
+    -r "cd '${startdir}'; startup; cd '${workdir}'; obj = ${classname}(); obj.run_worker('${savedir}',$$1); exit;" \\
+    >| "${logdir}/runworker_$${1}.log" 2>&1 & );
 
+echo "Running with pid $${pid}."
 """)
 
 # Write scripts according to current config
