4a02882ecb4f400d4474fa1600e945a05b1c1d4e
Jonathan H <Jonathan.hadida@dtc.ox.ac.uk>
2016-10-01 19:48:51 +0100

    Allow style format to be overriden by input filename

--- +dk/+ui/+fig/export.m
+++ +dk/+ui/+fig/export.m
@@ -3,12 +3,20 @@ function style = export( fig, fname, style )
 % Export a figure "fig" to an image named "fname" using the export style (string or struct) "style".
 % If the style is specified as a string, then the style is loaded from Matlab and this might throw 
 % an error if it doesn't exist. The default style is used if nothing is specified.
+%
+% Note that the Format field fo the style will is overriden if the input filename contains an extension.
 % 
 % Contact: jhadida [at] fmrib.ox.ac.uk
 
     if nargin < 3,    style = 'default'; end
     if ischar(style), style = hgexport('readstyle',style); end
 
+    [~,~,ext] = fileparts(fname);
+    ext = dk.str.lstrip(ext,'.');
+    if ~isempty(ext)
+        style.Format = ext;
+    end
+    
     hgexport( fig, fname, style );
     
 end
