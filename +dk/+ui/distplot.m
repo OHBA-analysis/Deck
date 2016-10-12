628058907cb6ac5773abdc08816d538c63445159
Jonathan H <Jonathan.hadida@dtc.ox.ac.uk>
2016-10-12 20:20:49 +0100

    Improve distribution plot

--- +dk/+ui/distplot.m
+++ +dk/+ui/distplot.m
@@ -1,12 +1,58 @@
-function [dist,xtic] = distplot( data, wid )
+function dist = distplot( data, varargin )
+%
+% [dist,xtic] = distplot( data, Name, Value )
+%
+% Distribution plot (similar to boxplot, but using ksdensity).
+% 
+% INPUT
+%
+%       data  Either a nxp matrix or a 1xp cell.
+%               If a matrix, each column is a sample (ie rows are observations).
+%               If a cell, each element is vectorised and considered as a 1-d sample.
+%
+%
+%    'Width'  Width of each distribution in the final plot, typically <1 (default: 0.7).
+%    'Theme'  For now only the theme 'orange is available'.
+%    'Range'  Support of the distribution as a 1x2 interval (default: []).
+%               By default, the interval is determined automatically by ksdensity.
+%   'NumPts'  Number of points to use for density estimation (default: 51).
+%
+% OUTPUT
+%   
+%       dist  Structure with fields:
+%               .x  Support of the density estimate
+%               .y  Density estimate
+%               .m  Median of the sample
+%               .a  Mean of the sample
+%
+% JH
 
-    % maximum width of the distribution for each column
-    if nargin < 2, wid = 0.7; end
+    opt = dk.obj.kwArgs(varargin{:});
+    
+    % parse options
+    opt_width = opt.get('width',0.7);
+    opt_theme = opt.get('theme','orange');
+    opt_range = opt.get('range',[]);
+    opt_supp  = opt.get('support',[]);
+    opt_npts  = opt.get('numpts',51);
 
     % colors used for drawing
-    color.box = hsv2rgb([30/360 1 0.9]); % orange
-    color.med = hsv2rgb([30/360 1 0.1]); % black
-    color.avg = hsv2rgb([200/360 1 0.9]/100); % blue
+    switch lower(opt_theme)
+        case 'orange'
+            theme.box = hsv2rgb([30/360 1 0.9]); % orange
+            theme.med = hsv2rgb([30/360 1 0.1]); % black
+            theme.avg = hsv2rgb([200/360 1 0.9]/100); % blue
+    end
+    
+    % process ksdensity options
+    if isempty(opt_range)
+        ksarg = { [], 'NumPoints', opt_npts };
+    else
+        ksarg = { linspace( opt_range(1), opt_range(2), opt_npts ) };
+    end
+    if ~isempty(opt_supp)
+        ksarg = [ ksarg, { 'Support', opt_supp } ];
+    end
 
     % number of columns
     if iscell(data)
@@ -14,26 +60,26 @@ function [dist,xtic] = distplot( data, wid )
     else
         nd = size(data,2);
     end
+    assert( nd > 0, 'Empty dataset in input.' );
     
     % compute and draw distributions
     dist = cell(1,nd);
-    step = ceil(wid);
-    xtic = 0.5 + step * (0:nd-1);
+    xtic = ceil(opt_width) * (0.5 + (0:nd-1));
     
     q99 = -Inf;
     q01 =  Inf;
     
     for i = 1:nd
-        [dist{i},v] = distribution( data, i );
-        plot_distribution( dist{i}, xtic(i), wid, color );
+        [dist{i},v] = density_estimation( data, i, ksarg );
+        plot_distribution( dist{i}, xtic(i), opt_width, theme );
         
         q99 = max( q99, prctile(v,99) );
         q01 = min( q01, prctile(v,1) );
     end
     
     % adjust y-axis limits
-    q99 = 1.1*q99;
-    q01 = 0.9*q01;
+    q99 = 1.2*q99;
+    q01 = 0.8*q01;
     
     % prevent drawing over, and set tick labels
     hold off; ylim([q01 q99]);
@@ -44,15 +90,15 @@ function [dist,xtic] = distplot( data, wid )
 
 end
 
-function [dist,v] = distribution( dat, k )
+function [dist,v] = density_estimation( dat, k, arg )
 
     if iscell(dat)
         v = dat{k}(:);
     else
         v = dat(:,k);
     end
-
-    [dist.y,dist.x] = ksdensity(v,[],'NumPoints',50);
+    
+    [dist.y,dist.x] = ksdensity(v,arg{:});
     dist.m = median(v);
     dist.a = mean(v);
     
@@ -71,6 +117,5 @@ function h = plot_distribution( dist, loc, wid, col )
     h.m = plot( loc+mw*[-1,1], dist.m*[1,1], '-', 'Color', col.med, 'LineWidth', 2 );
 
     %h.a = plot( loc, dist.a, '+', 'Color', col.avg, 'MarkerSize', 7 );
-    %h.a = plot( loc, dist.a, 'r+' );
     
 end
