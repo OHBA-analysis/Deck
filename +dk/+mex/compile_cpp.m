7291547493aac8c315b3f1eaf7e1cf9a92738df3
Jonathan H <Jonathan.hadida@dtc.ox.ac.uk>
2016-11-10 12:00:45 +0000

    Rename Mex

--- +dk/+mex/compile_cpp.m
+++ +dk/+mex/compile_cpp.m
@@ -1,6 +1,6 @@
 function compile_cpp( comp, varargin )
     
     comp.mex_file = false;
-    dk.mx.compile( comp, varargin{:} );
+    dk.mex.compile( comp, varargin{:} );
 
 end
--- +dk/+mex/compile_mex.m
+++ +dk/+mex/compile_mex.m
@@ -1,10 +1,10 @@
 function compile_mex( comp, varargin )
 
     if nargin == 1 && ischar(comp)
-        dk.mx.compile( dk.mx.compiler(), comp );
+        dk.mex.compile( dk.mex.compiler(), comp );
     else
         comp.mex_file = true;
-        dk.mx.compile( comp, varargin{:} );
+        dk.mex.compile( comp, varargin{:} );
     end
     
 end
