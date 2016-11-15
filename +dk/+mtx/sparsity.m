8a37ac942d9e2a897e0216b8fc911b2054d47081
Jonathan H <Jonathan.hadida@dtc.ox.ac.uk>
2016-11-15 10:14:16 +0000

    Set epsilon correctly

--- +dk/+mtx/sparsity.m
+++ +dk/+mtx/sparsity.m
@@ -4,7 +4,7 @@ function s = sparsity( M, thresh )
 % The sparsity is evaluated relatively to the number of elements with magnitude < thresh.
 % The default threshold is 1e-6.
 
-    if nargin < 2, thresh = 1e-6; end
+    if nargin < 2, thresh = eps; end
     
     s = sum(abs(M(:)) < thresh) / numel(M);
 
