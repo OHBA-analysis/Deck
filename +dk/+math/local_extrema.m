4754ef0335bd55fc98a2731d0a96c4b092888312
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2016-11-26 02:54:55 +0000

    Handle strict/nonstrict extrema

--- +dk/+math/local_extrema.m
+++ +dk/+math/local_extrema.m
@@ -1,28 +1,33 @@
-function [lmin,lmax] = local_extrema( x, inc_endpoints )
+function [lmin,lmax] = local_extrema( x, inc_endpoints, strict )
 %
-% [lmin,lmax] = LOCAL_EXTREMA( data )
+% [lmin,lmax] = LOCAL_EXTREMA( data, inc_endpoints=false, strict=false )
 %
 % This function is an extremely fast local extrema finder (returns indices).
 % In the case of plateau in the data, the function returns the index of the _first_ point.
 %
 % Example:
 %
-%   data = [25 8 15 5 5 10 10 3 1 20 7];
+%   data = [25 8 15 5 5 10 10 10 3 1 20 7];
 %   dk.math.local_extrema(data);
 %
 % JH
 
     if nargin < 2, inc_endpoints = false; end
+    if nargin < 3, strict = false; end
     assert( isvector(x) && isnumeric(x), 'Input data should be a numeric vector.' );
-
-    x  = x(:);
-    dx = diff(x,1,1);
+    if strict
+        comp = @(x) x < 0;
+    else
+        comp = @(x) x <= 0;
+    end
+    
+    dx = diff(x(:),1,1);
     
     if inc_endpoints
-        zc = [dx(1:end-1) .* dx(2:end) <= 0; 1];
+        zc = [comp(dx(1:end-1) .* dx(2:end)); 1];
         le = [ -dx(1); zc .* dx ];
     else
-        zc = [dx(1:end-1) .* dx(2:end) <= 0; 0];
+        zc = [comp(dx(1:end-1) .* dx(2:end)); 0];
         le = [ 0; zc .* dx ];
     end
     
@@ -38,7 +43,10 @@ function [lmin,lmax] = local_extrema( x, inc_endpoints )
         plot( lmax, x(lmax), 'ro' ); hold off;
         
         title('Showing local extrema');
-        legend('Data','Local min','Local max');
+        L = {'Data'};
+        if ~isempty(lmin), L{end+1}='Local min'; end
+        if ~isempty(lmax), L{end+1}='Local max'; end
+        legend(L{:});
         
     end
     
