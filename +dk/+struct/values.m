a781b649607efa27df8e2ff52340ad2536da7a49
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2016-11-29 18:51:36 +0000

    Add optional ordered filter on output

--- +dk/+struct/values.m
+++ +dk/+struct/values.m
@@ -1,13 +1,19 @@
-function v = values( s )
+function v = values( s, order )
 %
-% v = dk.struct.values( s )
+% v = dk.struct.values( s, order={} )
 %
 % Returns a N x M cell-array where N is the size of the input struct-array and M is the number of fields.
 % Cell (i,j) contains the value of j^th field in the i^th structure.
 %
+% If order is specified as a cell-string of fieldnames, then the order of output columns is set accordingly.
+%
 % JH
 
     f = fieldnames(s);
+    if nargin > 1
+        f = intersect( order, f, 'stable' );
+    end
+    
     nstruct = numel(s);
     nfields = numel(f);
     v = cell( nstruct, nfields );
