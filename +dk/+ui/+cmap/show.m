44f5fa6c1d3d8451e61d0b394f32417499b1b6e4
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2016-12-20 02:18:10 +0000

    Allow to rotate the plot

--- +dk/+ui/+cmap/show.m
+++ +dk/+ui/+cmap/show.m
@@ -1,6 +1,15 @@
 function show( cmap, sz )
+%
+% dk.ui.cmap.show( cmap, sz=[600 100] )
+%
 
     if nargin < 2, sz=[600 100]; end
-    imshow(imresize(flipud(reshape( cmap, [],1,3 )),sz,'nearest'));
+    
+    cmap = flipud(reshape( cmap, [],1,3 ));
+    if sz(2) > sz(1)
+        cmap = cmap';
+    end
+    
+    imshow(imresize(cmap,sz,'nearest'));
 
 end
