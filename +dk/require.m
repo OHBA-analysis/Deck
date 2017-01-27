c5c4ec714b4637705f917c4048ca6f4d60f07362
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2017-01-27 12:27:31 +0000

    Add utility to check that application is installed on system

---
+++ +dk/require.m
@@ -0,0 +1,11 @@
+function ok = require( name )
+%
+% Require application to be installed on the system.
+% This shorthand is mainly to get rid of the output printed "system" if it is called with only one output.
+%
+% JH
+
+    [s,m] = system( ['which ' name] );
+    ok = ~logical(s);
+    
+end
