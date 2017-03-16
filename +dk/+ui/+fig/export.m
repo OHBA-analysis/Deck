d4485fc3654ddc3906bf962e5793ab4014dd41d8
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2017-03-16 18:53:56 +0000

    Help text

--- +dk/+ui/+fig/export.m
+++ +dk/+ui/+fig/export.m
@@ -1,12 +1,14 @@
 function style = export( fig, fname, style )
 % 
+% style = dk.ui.fig.export( fig, fname, style )
+%
 % Export a figure "fig" to an image named "fname" using the export style (string or struct) "style".
 % If the style is specified as a string, then the style is loaded from Matlab and this might throw 
 % an error if it doesn't exist. 
 %
 % NOTE: Format field in the style is overriden if filename specifies the extension.
 % 
-% Contact: jhadida [at] fmrib.ox.ac.uk
+% JH
 
     if ischar(style), style = hgexport('readstyle',style); end
 
