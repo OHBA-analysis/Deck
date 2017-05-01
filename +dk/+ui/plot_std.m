eac71b812f0518e0b21f8d8b1646ac85d08974bf
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2017-05-01 17:32:38 +0100

    More flexible implementation

--- +dk/+ui/plot_std.m
+++ +dk/+ui/plot_std.m
@@ -5,7 +5,9 @@ function [ph,fh] = plot_std( x, y, yd, popts, fopts )
 % Plot the curve (x,y) on top of a background surface spanning (y-yd,y+yd).
 %
 % Required: x, y, yd (same size vectors)
-% Optional: popts, fopts (cell)
+% Optional: popts, fopts (cell of parameters forwarded to plot and fill, respectively)
+%
+% yd can also be nx2 or 2xn, in which case it specifies the lower and upper bound of the surface.
 %
 % Example:
 %   x  = linspace( 0, 2*pi, 100 ); 
@@ -13,7 +15,7 @@ function [ph,fh] = plot_std( x, y, yd, popts, fopts )
 %   yd = .05 + abs(y)/10; 
 %   dk.ui.plot_std(x,y,yd);
 %
-% Contact: jhadida [at] fmrib.ox.ac.uk
+% JH
 
 
     color_blue = lab2rgb([60 -5 -30]);
@@ -23,15 +25,24 @@ function [ph,fh] = plot_std( x, y, yd, popts, fopts )
         popts = {'LineWidth',1.5,'Color',color_blue};
     end
     if nargin < 5
-        fopts = {'LineWidth',1,'EdgeColor',color_red,'FaceAlpha',.7};
+        fopts = {'LineWidth',1,'EdgeColor',color_red,'FaceAlpha',0.6};
     end
 
-    x = x(:); y = y(:); yd = yd(:);
+    n = numel(x);
+    assert( numel(y)==n, 'y size mismatch.' );
+    x = x(:); y = y(:);
     
-    assert( (numel(x) == numel(y)) && (numel(x) == numel(yd)), 'Input size mismatch.' );
-    assert( all(yd >= 0), 'Deviations should be positive.' );
+    if numel(yd) == 2*n
+        assert( ismatrix(yd) && any(size(yd)==2), 'yd should be 2xn or nx2.' );
+        if size(yd,1)==2, yd = yd'; end % make it nx2
+    else
+        assert( numel(yd)==n, 'yd size mismatch.' );
+        yd = yd(:);
+        assert( all(yd >= 0), 'Deviations should be positive.' );
+        yd = [y-yd,y+yd];
+    end
     
-    fh = fill( vertcat(x,flipud(x)), vertcat(y+yd,flipud(y-yd)), color_red, fopts{:} ); hold on;
+    fh = fill( vertcat(x,flipud(x)), vertcat(yd(:,1),flipud(yd(:,2))), color_red, fopts{:} ); hold on;
     ph = plot( x, y, popts{:} ); hold off;
 
 end
