14838ce57ed83ae9a61119309890a110995ba50e
Jonathan Hadida <jhadida@fmrib.ox.ac.uk>
2017-05-23 20:14:21 +0100

    update

--- +dk/+math/stats.m
+++ +dk/+math/stats.m
@@ -1,17 +1,19 @@
-function s = stats( X, dim )
+function s = stats( X, dim, red )
 %
-% s = dk.math.stats( X, dim=1 )
+% s = dk.math.stats( X, dim=1, red=forward )
 %
 % Compute basic stats (mean, std, skewness) along a given dimension.
+% red can be used eg to squeeze the results if needed (red=@squeeze).
 %
 % JH
 
     if nargin < 2, dim = 1; end
+    if nargin < 3, red = @(x) x; end
 
     s = struct( ...
-        'mean', squeeze(nanmean(X,dim)), ...
-        'sdev', squeeze(nanstd(X,[],dim)), ...
-        'skew', dk.math.nanreplace(squeeze(skewness(X,[],dim)),0) ...
+        'mean', red(nanmean(X,dim)), ...
+        'sdev', red(nanstd(X,[],dim)), ...
+        'skew', dk.math.nanreplace(red(skewness(X,[],dim)),0) ...
     );
 
 end
