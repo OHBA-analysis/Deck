51a0b6863f184115853a99de6b6fba0d69aad3c1
Jonathan H <Jhadida@fmrib.ox.ac.uk>
2017-07-23 11:24:55 +0100

    Filter Matlab path

--- +dk/+fs/realpath.m
+++ +dk/+fs/realpath.m
@@ -6,6 +6,7 @@ function p = realpath(name)
     % This does not work for file links:
     %[s,p] = system(sprintf('cd "%s" && pwd -P',name)); 
     %dk.assert( s==0, 'Path not found: "%s".', name );
-    %p = deblank(p);
+    
+    p = deblank(p);
     
 end
