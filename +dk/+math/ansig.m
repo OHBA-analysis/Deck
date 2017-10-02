b1372ae3942728c1e8c5e0c64bf1e16f98db4927
Jonathan H <Jhadida@fmrib.ox.ac.uk>
2017-10-02 18:57:45 +0100

    Plot result if no output collected

--- +dk/+math/ansig.m
+++ +dk/+math/ansig.m
@@ -12,6 +12,7 @@ function varargout = ansig( x, fs )
 % OUTPUTS:
 %
 %   env    Oscillatory envelopes for each signal.
+%          NOTE: the envelope is NOT re-meaned.
 %   phi    Instantaneous phase for each signal.
 %   frq    Instantaneous frequency for each signal 
 %          Computed only if required, in cycles/unit unless fs is defined.
@@ -21,6 +22,9 @@ function varargout = ansig( x, fs )
 %   [env,~] = dk.math.ansig(x); % real-valued envelope returned 
 %   as = dk.math.ansig(x); % complex-valued analytic signal returned
 %
+% If no output is collected, a new figure is opened, showing the trajectory of the analytic
+% signal in the complex plane.
+%
 % JH
     
     if nargin < 2, fs=1; end
@@ -28,6 +32,12 @@ function varargout = ansig( x, fs )
     sig = hilbert(dk.bsx.sub( x, mean(x,1) ));
     
     switch nargout
+        case 0
+            figure;
+            [x,y] = pol2cart( angle(sig), abs(sig) );
+            plot( x, y, 'k-' );
+            title('Complex analytic signal');
+            axis equal; grid on;
         case 1
             varargout = {sig};
         case 2
