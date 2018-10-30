69a81eba2aff76c3713ceadb2d1776d8d4dab851
Jonathan H <jonathan.hadida@psych.ox.ac.uk>
2018-10-30 13:19:43 +0000

    Move require

--- +dk/+env/require.m
+++ +dk/+env/require.m
@@ -1,6 +1,6 @@
 function ok = require( name )
 %
-% ok = dk.require( name )
+% ok = dk.env.require( name )
 %
 % Require application to be installed on the system.
 % This shorthand is mainly to get rid of the output printed "system" if it is called with only one output.
