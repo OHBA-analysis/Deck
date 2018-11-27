5efaff16d211e2618090da8e0b4deefe1c64a2de
Jonathan Hadida <jhadida87@gmail.com>
2018-11-27 17:32:49 +0000

    Make install script work

--- jmx/install.sh
+++ jmx/install.sh
@@ -5,17 +5,17 @@ HERE=$(dirname "${BASH_SOURCE}")
 # clone Armadillo repository
 ARMA_NAME=armadillo-code
 ARMA_REPO=https://gitlab.com/conradsnicta
-ARMA_RELP=inc
 
-ARMA_TARGET=$HERE/$ARMA_RELP
-ARMA_FOLDER=$ARMA_TARGET/$ARMA_NAME
+pushd "$HERE/inc"
+ARMA_FOLDER=$ARMA_NAME
 ARMA_URL=$ARMA_REPO/${ARMA_NAME}.git
 
 if [ ! -d "$ARMA_FOLDER" ]; then
-    git clone "$ARMA_URL" "$ARMA_TARGET"
+    git clone "$ARMA_URL"
     (( $? == 0 )) || echo "ERROR: could not clone repo '$ARMA_URL' into folder '$ARMA_FOLDER'"
 else
     echo "Found Armadillo in: $ARMA_FOLDER"
     echo "    (if the folder is empty / corrupted, delete it first)"
 fi
+popd
 
