0ac8c05a1487f6162f59e1fd4a03871ef249066c
Jonathan H <jonathan.hadida@psych.ox.ac.uk>
2019-02-05 22:54:35 +0000

    add dirname

---
+++ +dk/+fs/dirname.m
@@ -0,0 +1,29 @@
+function d = dirname( x, n )
+%
+% d = dk.fs.dirname( x, n=0 )
+%
+% JH
+
+    if nargin < 2, n=0; end
+    
+    % trim and handle empty strings
+    x = strtrim(x);
+    if isempty(x)
+        x = pwd();
+    end
+    
+    % if the last part has an extension, assume it is a file and remove it
+    x = strsplit( x, filesep );
+    if ~isempty(regexp( x{end}, '\.\S+$' ))
+        x = x(1:end-1);
+    end
+    
+    % assume pwd if nothing remains
+    if isempty(x)
+        x = strsplit( pwd, filesep );
+    end
+    
+    % remove requested number of folders
+    d = strjoin( x(1:end-n), filesep );
+
+end
