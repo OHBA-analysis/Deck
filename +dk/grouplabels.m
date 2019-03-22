b7291270871f100865f9550cb86e5ac0d3c9c88f
Jonathan H <Jhadida@fmrib.ox.ac.uk>
2019-03-22 15:21:10 +0000

    Add groupunique

--- +dk/grouplabels.m
+++ +dk/grouplabels.m
@@ -1,36 +1,37 @@
-function [G,u] = grouplabels(L,n,t)
+function G = grouplabels(L,n,t)
 %
-% [G,u] = dk.grouplabels(L)
-% [G,u] = dk.grouplabels(L,n)
-% [G,u] = dk.grouplabels(L,n,t)
+% G = dk.grouplabels(L)
+% G = dk.grouplabels(L,n)
+% G = dk.grouplabels(L,n,t)
 %
 % For each unique label in L, find indices of elements equal to this label.
 % Output G is a 1xn cell:
 %   - if n is not specified, then n=max(L) by default;
 %   - otherwise, if n < max(L), then G only contains the first n groups.
-% Output u is a 1xn vector with the corresponding labels.
 %
 % If t is specified, then:
 %   - L should be sorted, and 
 %   - t is a 1xn vector, such that t(i) is the index of the first element
 %     in L equal to the i^th label.
 %
-% LABELS SHOULD BE POSITIVE INTEGERS
 %
 % Complexity is m log(m), where m=length(L).
 % In theory, could be improved to linear complexity using integer sort.
 % This version is already ~10-20x faster than using Matlab's splitapply.
 %
-% Example: group elements by rounded value
+% NOTE:
+%   - LABELS ARE ASSUMED TO BE CONSECUTIVE INTEGERS STARTING AT 1.
+%   - Consequently, group G{i} has label i.
+%   - If that is not the case, consider using dk.groupunique instead.
 %
-%   x = rand(1,1000);
-%   [u,~,L] = unique(round(x,2));
-%   G = dk.grouplabels(L,101);
+% See also: dk.groupunique
 %
 % JH
 
-    L = L(:); % make a col
-    if nargin < 2, n = max(L); end
+    L = L(:);
+    if nargin < 2 || isempty(n)
+        n = max(L); 
+    end
     
     % compute t
     if nargin < 3
@@ -63,7 +64,7 @@ function [G,u] = grouplabels(L,n,t)
     end 
     
     % get corresponding labels (empty groups are assigned NaN)
-    u = L(s(t(1:n)))';
-    u(diff(t) == 0) = nan;
+    %u = L(s(t(1:n)))';
+    %u(diff(t) == 0) = nan;
 
 end
