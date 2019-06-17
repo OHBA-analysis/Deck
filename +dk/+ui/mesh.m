b480d30d95fcd8a8c28fcd327081993ccacc7a00
Jonathan H <jonathan.hadida@ohba.ox.ac.uk>
2019-06-17 15:41:09 +0100

    Better doc and options

--- +dk/+ui/mesh.m
+++ +dk/+ui/mesh.m
@@ -2,14 +2,13 @@ function h = mesh(m,varargin)
 %
 % h = dk.ui.mesh( mesh, args... )
 %
-% Draw input mesh as a patch with specified options.
-%
 % The input mesh should be a struct { faces, vertices }.
+% This mesh is drawn as a patch with specified options:
 %
-% The option FaceVertexCData automatically converts face-color specification to
-% vertex-color specification if needed.
+%   - Additional options FaceColors and VertexColors
+%   - FaceVertexCData automatically converts face-colors to vertex-colors
+%   - FaceColor defaults to 'flat'
 %
-% 
 % See also: dk.ui.face2vertex, patch
 %
 % JH
@@ -24,20 +23,24 @@ function h = mesh(m,varargin)
     nf = size( f, 1 );
     nv = size( v, 1 );
     
+    auto_facecolor = true;
     for i = 1:2:nargin-1
         switch lower(varargin{i})
-            case 'vertexcolor'
-                varargin{i} = 'FaceVertexCData';
             case 'facecolor'
-                if dk.is.rgb(varargin{i+1},nf)
-                    varargin{i} = 'FaceVertexCData';
-                    varargin{i+1} = dk.ui.face2vertex( varargin{i+1}, f, nv );
-                end
+                auto_facecolor = false;
+            case 'vertexcolors'
+                varargin{i} = 'FaceVertexCData';
+            case 'facecolors'
+                varargin{i} = 'FaceVertexCData';
+                varargin{i+1} = dk.ui.face2vertex( varargin{i+1}, f, nv );
             case 'facevertexcdata'
                 if size(varargin{i+1},1) ~= nv
                     varargin{i+1} = dk.ui.face2vertex( varargin{i+1}, f, nv );
                 end
         end
+    end
+    if auto_facecolor
+        varargin = [varargin, {'FaceColor','flat'}];
     end
     
     % draw patch
