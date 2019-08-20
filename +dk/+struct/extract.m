662f71bc00b0757d90142624ad21ef8e5364d4ba
Jonathan H <jonathan.hadida@ohba.ox.ac.uk>
2019-08-20 16:29:03 +0100

    Add function to extract fields from structure to variables

--- +dk/+struct/extract.m
+++ +dk/+struct/extract.m
@@ -1,13 +1,21 @@
-function s = extract( s, varargin )
+function varargout = extract( in, varargin )
 %
-% s = extract( s, field1, field2, ... )
+% [v1,v2, ...] = dk.struct.extract( in, field1, field2, ... )
 %
-% Remove all but specified fields from input structure s.
-% Works with struct-arrays.
+% Forward the values of specified fields to output variables.
+% Analogous to a deal() function for scalar structures.
 %
 % JH
 
-    x = dk.unwrap(varargin);
-    s = rmfield( s, setdiff( fieldnames(s), x ) );
+    f = dk.unwrap(varargin);
+    n = numel(f);
+    
+    assert( iscellstr(f), 'Fieldnames should be strings.' );
+    assert( dk.is.struct(in,f), 'This function only works with scalar struct.' );
+    
+    varargout = cell(1,n);
+    for i = 1:n
+        varargout{i} = in.(f{i});
+    end
 
 end
