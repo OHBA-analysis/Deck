687dc470916edf2f28d00c61a1b16446fc221c70
Jonathan H <Jhadida@fmrib.ox.ac.uk>
2020-02-21 09:54:44 +0000

    Optional length of partition

--- +dk/+num/isperm.m
+++ +dk/+num/isperm.m
@@ -1,16 +1,14 @@
-function yes = isperm(p)
+function yes = isperm(p,len)
 %
 % yes = dk.num.isperm(p)
+% yes = dk.num.isperm(p,n)
 %
-% Checks whether input vector p is a permutation.
-%
-% Input:
-%   p a permutation vector or matrix
-%   If p has N elements, then p should contain a permutation of 1:N
+% Checks whether input vector p is a permutation of 1:n.
+% By default, n = numel(p).
 %
 % JH
 
-    len    = numel(p);
+    if nargin < 2, len = numel(p); end
     counts = accumarray( p(:), 1, [len,1] );
-    yes    = all(counts == 1);
+    yes = all(counts == 1);
 end
